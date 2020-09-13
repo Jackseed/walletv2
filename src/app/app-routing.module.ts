e558b95ce526a85003b12525cdd63216acf9a689
Jackseed <vinkho.c@gmail.com>
2020-09-13 16:09:50 +0200

    first deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { MnemonicComponent } from './wallet/containers/mnemonic/mnemonic.component';
-import { VerificationComponent } from './wallet/containers/verification/verification.component';
-import { WalletComponent } from './wallet/containers/wallet/wallet.component';
-import { TransactionComponent } from './wallet/containers/transaction/transaction.component';
-import { HomeComponent } from './wallet/containers/home/home.component';
-
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { MnemonicComponent } from "./wallet/containers/mnemonic/mnemonic.component";
+import { VerificationComponent } from "./wallet/containers/verification/verification.component";
+import { WalletComponent } from "./wallet/containers/wallet/wallet.component";
+import { TransactionComponent } from "./wallet/containers/transaction/transaction.component";
+import { HomeComponent } from "./wallet/containers/home/home.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'mnemonic', component: MnemonicComponent },
-  { path: 'verification', component: VerificationComponent },
-  { path: 'wallet', component: WalletComponent },
-  { path: 'transaction', component: TransactionComponent }
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "mnemonic", component: MnemonicComponent },
+  { path: "verification", component: VerificationComponent },
+  { path: "wallet", component: WalletComponent },
+  { path: "transaction", component: TransactionComponent },
+  {
+    path: "**",
+    redirectTo: "/home",
+    pathMatch: "full"
+  }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
-
-
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { WalletModule } from './wallet/wallet.module';
-import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { environment } from 'src/environments/environment';
-import { UiModule } from './ui/ui.module';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { WalletModule } from "./wallet/wallet.module";
+import { AkitaNgDevtools } from "@datorama/akita-ngdevtools";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { environment } from "src/environments/environment";
+import { UiModule } from "./ui/ui.module";
+import { FormsModule } from "@angular/forms";
+import { AngularFireModule } from "@angular/fire";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDY75xixOc6B9j8i8HqhsfZgwQsYYk4A_A",
+  authDomain: "midas-wallet.firebaseapp.com",
+  databaseURL: "https://midas-wallet.firebaseio.com",
+  projectId: "midas-wallet",
+  storageBucket: "midas-wallet.appspot.com",
+  messagingSenderId: "783434171001",
+  appId: "1:783434171001:web:540db33f75ba3f4b80caa8"
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     AppRoutingModule,
     WalletModule,
     BrowserAnimationsModule,
@@ -27,4 +35,4 @@ import { FormsModule } from '@angular/forms';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
